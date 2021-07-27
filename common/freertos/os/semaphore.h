6822e6777502ab8b22f8079e6f265ebc2664ec2e
Stephane Viau <stephane.viau@nxp.com>
2021-07-27 05:07:59 +0000

    osal: Create os/semaphore layer
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

---
+++ common/freertos/os/semaphore.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _FREERTOS_SEMAPHORE_H_
+#define _FREERTOS_SEMAPHORE_H_
+
+#include <stdint.h>
+
+#include "os.h"
+#include "os/assert.h"
+
+#include "semphr.h"
+
+typedef struct {
+    SemaphoreHandle_t handle;
+    StaticSemaphore_t buffer;
+} os_sem_t;
+
+static inline int os_sem_init(os_sem_t *sem, uint32_t init_count)
+{
+    /*
+     * In order to avoid dynamic allocation, the address of the semaphore buffer
+     * is passed in and will be used to hold the semaphore structure.
+     */
+    sem->handle = xSemaphoreCreateCountingStatic((UBaseType_t)UINT_MAX,
+                              (UBaseType_t) init_count, &sem->buffer);
+
+    return (sem->handle == NULL) ? -1 : 0;
+}
+
+static inline int os_sem_destroy(os_sem_t *sem)
+{
+    vSemaphoreDelete(sem->handle);
+
+    return 0;
+}
+
+static inline int os_sem_give(os_sem_t *sem)
+{
+    BaseType_t xYieldRequired;
+    BaseType_t ret;
+
+    ret = xSemaphoreGiveFromISR(sem->handle, &xYieldRequired);
+
+    /* If xYieldRequired was set to true, we should yield */
+    portYIELD_FROM_ISR( xYieldRequired );
+
+    return (ret == pdTRUE) ? 0 : -1;
+}
+
+static inline int os_sem_take(os_sem_t *sem, uint32_t timeout_ms)
+{
+    BaseType_t ret;
+    TickType_t t;
+
+    if (timeout_ms == OS_SEM_TIMEOUT_MAX)
+        t = portMAX_DELAY;
+    else
+        t = pdMS_TO_TICKS(timeout_ms);
+
+    ret = xSemaphoreTake(sem->handle, t);
+
+    return (ret == pdTRUE) ? 0 : -1;
+}
+
+#endif /* #ifndef _FREERTOS_SEMAPHORE_H_ */
---
+++ common/os/semaphore.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _COMMON_SEMAPHORE_H_
+#define _COMMON_SEMAPHORE_H_
+
+#include <limits.h>
+#include <stdint.h>
+
+#include "os.h"
+
+#define OS_SEM_TIMEOUT_MAX    UINT_MAX
+
+/**
+ *  Flags used in some of below APIs,
+ *  essentially used for FreeRTOS ; ignored for Zephyr implementation
+ */
+#define OS_SEM_FLAGS_ISR_CONTEXT    (1 << 0)
+
+#if defined(OS_ZEPHYR)
+  #include "zephyr/os/semaphore.h"
+#elif defined(OS_FREERTOS)
+  #include "freertos/os/semaphore.h"
+#endif
+
+int os_sem_init(os_sem_t *sem, uint32_t init_count);
+int os_sem_give(os_sem_t *sem, uint32_t flags);
+int os_sem_take(os_sem_t *sem, uint32_t flags, uint32_t timeout_ms);
+int os_sem_destroy(os_sem_t *sem);
+
+#endif /* #ifndef _COMMON_SEMAPHORE_H_ */
