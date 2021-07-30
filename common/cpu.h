8c33f3b64c39ec10d9db481ae25584670ab9ac02
Stephane Viau <stephane.viau@nxp.com>
2021-07-30 08:10:36 +0000

    common: Create cpu.h header file
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

---
+++ common/cpu.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _COMMON_CPU_H_
+#define _COMMON_CPU_H_
+
+#include "os.h"
+#include "os/stdio.h"
+
+#ifdef OS_ZEPHYR /* TODO: Implement cache invalidation with OS-independant code */
+#include <zephyr.h>
+#include <kernel.h>
+#include <cache.h>
+#endif
+
+static inline void os_invd_dcache_all()
+{
+#ifndef OS_ZEPHYR /* TODO: Implement cache invalidation with OS-independant code */
+    static int warn_once = 0;
+
+    if (!warn_once++)
+        os_printf("WARNING:  TODO: Flush D-Cache -> HRPN-90 \n\r ");
+#else
+    sys_cache_data_all(K_CACHE_INVD);
+#endif
+}
+
+static inline void os_invd_icache_all()
+{
+    __asm volatile ("IC IALLUIS");
+}
+
+#endif /* #ifndef _COMMON_CPU_H_ */
