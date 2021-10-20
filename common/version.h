8ecbcd6fdd1a4a66c080ce6ee7b7817953f73f1f
Stephane Viau <stephane.viau@nxp.com>
2021-10-20 16:05:25 +0000

    common: Add version file
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

---
+++ common/version.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2021 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define VERSION_MAJOR 2 /* eg API breaking */
+#define VERSION_MDIUM 0 /* Feature  update */
+#define VERSION_MINOR 0 /* Bufix /  Hotfix */
+
+
+#define _STRINGIFY(x) #x
+#define STRINGIFY(x)  _STRINGIFY(x)
+
+#define VERSION STRINGIFY(VERSION_MAJOR)"."STRINGIFY(VERSION_MDIUM)"."STRINGIFY(VERSION_MINOR)
+
+#endif /* #ifndef _VERSION_H_ */
