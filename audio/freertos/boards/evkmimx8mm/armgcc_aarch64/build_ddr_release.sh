126727b13a92f461030c2935cd2232f72cdf87fc
Rui Sousa <rui.sousa@nxp.com>
2021-12-08 22:16:52 +0100

    audio: Build a single image
    Now that we support choosing the audio mode at run time, remove the
    support for multiple images.
    
    Signed-off-by: Rui Sousa <rui.sousa@nxp.com>

--- audio/freertos/boards/evkmimx8mm/armgcc_aarch64/build_ddr_release.sh
+++ audio/freertos/boards/evkmimx8mm/armgcc_aarch64/build_ddr_release.sh
@@ -5,22 +5,10 @@
 # CODEC_DEVICE_TYPE="-DCODEC_DEVICE=hifiberry" will enable HiFiBerry Codec
 CODEC_DEVICE_TYPE="-DCODEC_DEVICE=hifiberry"
 
-do_clean() {
 if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
 if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-}
 
-do_build() {
-do_clean
-echo "build "$1""
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../mcux-sdk/tools/cmake_toolchain_files/armgcc_aarch64.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_release ${CODEC_DEVICE_TYPE} -DDEMO_TYPE="$1" .
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../mcux-sdk/tools/cmake_toolchain_files/armgcc_aarch64.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_release ${CODEC_DEVICE_TYPE} .
 make -j $(nproc)
-}
-
-do_build "play_dtmf"
-do_build "play_music"
-do_build "play_sine"
-do_build "rec_play"
-do_build "rec_play2"
