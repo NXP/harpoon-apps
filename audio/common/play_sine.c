eb78a541fe1839842c67eabd887aa4e42cdf4c3d
Rui Sousa <rui.sousa@nxp.com>
2021-12-08 15:56:08 +0100

    audio: play_sine: Add proper context structure
    Add context structure to play_sine
    
    Signed-off-by: Rui Sousa <rui.sousa@nxp.com>

--- audio/common/play_sine.c
+++ audio/common/play_sine.c
@@ -10,6 +10,7 @@
 #include "board.h"
 #include "os/assert.h"
 #include "os/semaphore.h"
+#include "os/stdlib.h"
 #include "sai_drv.h"
 #include "sine_wave.h"
 #include "sai_codec_config.h"
@@ -18,41 +19,48 @@
 #define PLAY_AUDIO_CHANNELS	2
 #define PLAY_AUDIO_BITWIDTH	32
 
-/* callback semaphore */
-static os_sem_t tx_semaphore;
-static os_sem_t rx_semaphore;
+struct sine_ctx {
+	/* callback semaphore */
+	os_sem_t tx_semaphore;
+	os_sem_t rx_semaphore;
+
+	struct sai_device dev;
+};
 
 static void rx_callback(const void *dev, void *userData)
 {
-	os_sem_give(&rx_semaphore, OS_SEM_FLAGS_ISR_CONTEXT);
+	struct sine_ctx *ctx = userData;
+
+	os_sem_give(&ctx->rx_semaphore, OS_SEM_FLAGS_ISR_CONTEXT);
 }
 
 static void tx_callback(const void *dev, void *userData)
 {
-	os_sem_give(&tx_semaphore, OS_SEM_FLAGS_ISR_CONTEXT);
+	struct sine_ctx *ctx = userData;
+
+	os_sem_give(&ctx->tx_semaphore, OS_SEM_FLAGS_ISR_CONTEXT);
 }
 
-static void play_sine(struct sai_device *dev)
+static int play_sine_run(void *handle)
 {
-	int err;
+	struct sine_ctx *ctx = handle;
+	struct sai_device *dev = &ctx->dev;
 	uintptr_t addr = (uintptr_t)sine_wave;
 	size_t len = sizeof(sine_wave);
-
-	os_printf("HifiBerry playing Sine wave is started (Sample Rate: %d Hz, Bit Width: %d bits)\r\n",
-			PLAY_AUDIO_SRATE, PLAY_AUDIO_BITWIDTH);
-	err = os_sem_init(&tx_semaphore, 0);
-	os_assert(!err, "tx semaphore initialization failed!");
+	int err;
 
 	while (1) {
 		err = sai_write(dev, (uint8_t *)addr, len);
 		if (!err) {
-			err = os_sem_take(&tx_semaphore, 0, OS_SEM_TIMEOUT_MAX);
+			err = os_sem_take(&ctx->tx_semaphore, 0, OS_SEM_TIMEOUT_MAX);
 			os_assert(!err, "Can't take the tx semaphore (err: %d)", err);
 		}
 	}
+
+	return 0;
 }
 
-static void sai_setup(struct sai_device *dev)
+static void sai_setup(struct sine_ctx *ctx)
 {
 	struct sai_cfg sai_config;
 
@@ -64,22 +72,52 @@ static void sai_setup(struct sai_device *dev)
 	sai_config.tx_sync_mode = DEMO_SAI_TX_SYNC_MODE;
 	sai_config.rx_sync_mode = DEMO_SAI_RX_SYNC_MODE;
 	sai_config.rx_callback = rx_callback;
+	sai_config.rx_user_data = ctx;
 	sai_config.tx_callback = tx_callback;
+	sai_config.tx_user_data = ctx;
 
-	sai_drv_setup(dev, &sai_config);
+	sai_drv_setup(&ctx->dev, &sai_config);
 }
 
-void play_sine_task(void *parameters)
+static void *play_sine_init(void *parameters)
 {
-	struct sai_device dev;
+	struct sine_ctx *ctx;
+	int err;
 
-	sai_setup(&dev);
+	ctx = os_malloc(sizeof(struct sine_ctx));
+	os_assert(ctx, "Playing DTMF failed with memory allocation error");
+
+	sai_setup(ctx);
 
 	codec_setup();
 	codec_set_format(DEMO_AUDIO_MASTER_CLOCK, PLAY_AUDIO_SRATE, PLAY_AUDIO_BITWIDTH);
 
-	play_sine(&dev);
+	os_printf("HifiBerry playing Sine wave is started (Sample Rate: %d Hz, Bit Width: %d bits)\r\n",
+			PLAY_AUDIO_SRATE, PLAY_AUDIO_BITWIDTH);
+	err = os_sem_init(&ctx->tx_semaphore, 0);
+	os_assert(!err, "tx semaphore initialization failed!");
 
-	for (;;)
-		;
+	return ctx;
+}
+
+static void play_sine_exit(void *handle)
+{
+	struct sine_ctx *ctx = handle;
+
+	/* Stop SAI */
+
+	codec_close();
+
+	os_free(ctx);
+}
+
+void play_sine_task(void *parameters)
+{
+	void *handle;
+
+	handle = play_sine_init(parameters);
+
+	play_sine_run(handle);
+
+	play_sine_exit(handle);
 }
