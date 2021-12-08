fa74efb89c1778d4545d9c4139f6253bf2e48211
Rui Sousa <rui.sousa@nxp.com>
2021-12-08 18:49:07 +0100

    audio: add command definition
    Add definition of audio commands:
    - start
    - stop
    
    Signed-off-by: Rui Sousa <rui.sousa@nxp.com>

--- common/libs/ctrl/hrpn_ctrl.h
+++ common/libs/ctrl/hrpn_ctrl.h
@@ -13,6 +13,10 @@ enum {
 	HRPN_CMD_TYPE_LATENCY_RUN = 0x0000,
 	HRPN_CMD_TYPE_LATENCY_STOP,
 	HRPN_RESP_TYPE_LATENCY = 0x0010,
+
+	HRPN_CMD_TYPE_AUDIO_RUN = 0x0100,
+	HRPN_CMD_TYPE_AUDIO_STOP,
+	HRPN_RESP_TYPE_AUDIO = 0x0110,
 };
 
 enum {
@@ -34,6 +38,24 @@ struct hrpn_resp_latency {
 	uint32_t status;
 };
 
+/* Audio application commands */
+struct hrpn_cmd_audio_run {
+	uint32_t type;
+	uint32_t id;
+	uint32_t frequency;
+	uint32_t period;
+};
+
+struct hrpn_cmd_audio_stop {
+	uint32_t type;
+};
+
+struct hrpn_resp_audio {
+	uint32_t type;
+	uint32_t status;
+};
+
+
 struct hrpn_cmd {
 	uint32_t type;
 };
@@ -48,6 +70,8 @@ struct hrpn_command {
 		struct hrpn_cmd cmd;
 		struct hrpn_cmd_latency_run latency_run;
 		struct hrpn_cmd_latency_stop latency_stop;
+		struct hrpn_cmd_audio_run audio_run;
+		struct hrpn_cmd_audio_stop audio_stop;
 	} u;
 };
 
@@ -55,6 +79,7 @@ struct hrpn_response {
 	union {
 		struct hrpn_resp resp;
 		struct hrpn_resp_latency latency;
+		struct hrpn_resp_audio audio;
 	} u;
 };
 
