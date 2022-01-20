acfc774dc8e5648138e8ac29edb5434a37aac8d8
Fabrice Goucem <fabrice.goucem@nxp.com>
2022-01-20 15:25:26 +0100

    audio: add support for evkmimx8mn
    Signed-off-by: Fabrice Goucem <fabrice.goucem@nxp.com>

---
+++ audio/freertos/boards/evkmimx8mn/app_mmu.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2021-2022 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _APP_MMU_H_
+#define _APP_MMU_H_
+
+#define APP_MMU_ENTRIES		\
+	MMU_REGION_FLAT_ENTRY("GPC",					\
+			      GPC_BASE, KB(64),				\
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+									\
+	MMU_REGION_FLAT_ENTRY("SAI5",					\
+			      I2S5_BASE, KB(64),			\
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+									\
+	MMU_REGION_FLAT_ENTRY("I2C3",					\
+			      I2C3_BASE, KB(64),			\
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+
+#endif /* _APP_MMU_H_ */
---
+++ audio/freertos/boards/evkmimx8mn/sai_clock_config.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2021-2022 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _SAI_CLOCK_CONFIG_H_
+#define _SAI_CLOCK_CONFIG_H_
+
+void board_clock_setup(uint8_t sai_id);
+
+#endif /* _SAI_CLOCK_CONFIG_H_ */
