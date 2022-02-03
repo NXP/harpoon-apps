4548a481504dd777c738e753fd63ae85102964f1
Rui Sousa <rui.sousa@nxp.com>
2022-02-03 14:12:20 +0100

    common: os: Add string.h
    Add os abstraction for string.h
    
    Signed-off-by: Rui Sousa <rui.sousa@nxp.com>

---
+++ common/freertos/os/string.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _FREERTOS_STRING_H_
+#define _FREERTOS_STRING_H_
+
+#include <string.h>
+
+#endif /* #ifndef _FREERTOS_STRING_H_ */
---
+++ common/os/string.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2022 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _COMMON_STRING_H_
+#define _COMMON_STRING_H_
+
+#if defined(OS_ZEPHYR)
+  #include "zephy/os/string.h"
+#elif defined(FSL_RTOS_FREE_RTOS)
+  #include "freertos/os/string.h"
+#endif
+
+#endif /* #ifndef _COMMON_STRING_H_ */
