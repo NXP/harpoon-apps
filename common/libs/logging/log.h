aa476e28ad2a89589f12dc3de4828d8d1e5b94d2
Stephane Viau <stephane.viau@nxp.com>
2022-02-23 15:54:00 +0000

    common: logging: Add raw level logging
    Used to avoid printing log buffer information such as the log level or
    the function name.
    
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

--- common/libs/logging/log.h
+++ common/libs/logging/log.h
@@ -43,6 +43,14 @@ extern log_level_t log_level_config;
 #define log_info(...)     log(INFO, __VA_ARGS__)
 #define log_debug(...)    log(DEBUG, __VA_ARGS__)
 
+#define log_raw(LEVEL, ...) \
+  do { \
+    if (log_level_config >= LOG_ ## LEVEL) \
+         os_printf(__VA_ARGS__); \
+  } while (0)
+
+#define log_raw_info(...) log_raw(INFO, __VA_ARGS__)
+
 /** Set current log level configuration */
 static inline void log_level_config_set(log_level_t log_level)
 {
