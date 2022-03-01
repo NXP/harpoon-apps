4be78413848ca20c9daadb33eaaca741c69d4f90
Rui Sousa <rui.sousa@nxp.com>
2022-03-01 17:48:48 +0100

    audio: freertos: fix mmu settings
    Use device type memory for all IO areas, not strongly ordered.
    Remove unused GPC area.
    
    Signed-off-by: Rui Sousa <rui.sousa@nxp.com>

--- audio/freertos/boards/evkmimx8mn/app_mmu.h
+++ audio/freertos/boards/evkmimx8mn/app_mmu.h
@@ -9,20 +9,16 @@
 #define _APP_MMU_H_
 
 #define APP_MMU_ENTRIES		\
-	MMU_REGION_FLAT_ENTRY("GPC",					\
-			      GPC_BASE, KB(64),				\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
-									\
 	MMU_REGION_FLAT_ENTRY("SAI5",					\
 			      I2S5_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
 	MMU_REGION_FLAT_ENTRY("I2C3",					\
 			      I2C3_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
 	MMU_REGION_FLAT_ENTRY("IOMUXC",					\
 			      IOMUXC_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 
 #endif /* _APP_MMU_H_ */
--- audio/freertos/boards/evkmimx8mp/app_mmu.h
+++ audio/freertos/boards/evkmimx8mp/app_mmu.h
@@ -11,34 +11,30 @@
 #define APP_MMU_ENTRIES		\
 	MMU_REGION_FLAT_ENTRY("AUDIOMIX",				\
 			      AUDIOMIX_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
 	MMU_REGION_FLAT_ENTRY("IOMUXC",					\
 			      IOMUXC_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
 	MMU_REGION_FLAT_ENTRY("GPIO5",					\
 			      GPIO5_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
 	MMU_REGION_FLAT_ENTRY("GPC",					\
 			      GPC_BASE, KB(64),				\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
 	MMU_REGION_FLAT_ENTRY("SAI3",					\
 			      I2S3_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
 	MMU_REGION_FLAT_ENTRY("I2C3",					\
 			      I2C3_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
 	MMU_REGION_FLAT_ENTRY("SAI5",					\
 			      I2S5_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
-									\
-	MMU_REGION_FLAT_ENTRY("IOMUXC",					\
-			      IOMUXC_BASE, KB(64),			\
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),	\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 
 #endif /* _APP_MMU_H_ */
