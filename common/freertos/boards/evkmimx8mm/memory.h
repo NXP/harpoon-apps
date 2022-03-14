df810ec65c1306c340b9082811436d57dafe457f
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2022-03-14 20:51:19 +0800

    Update i.MX8M{M,N} EVK FreeRTOS linker scripts
    Update the linker scripts to align with Jailhouse cell configuration.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- common/freertos/boards/evkmimx8mm/memory.h
+++ common/freertos/boards/evkmimx8mm/memory.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 NXP
+ * Copyright 2021-2022 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -11,7 +11,7 @@
 #define HYPERVISOR_COMM_BASE	0x80000000
 #define PCI_CFG_BASE		0xbb800000
 #define PCI_IVSHMEM_BASE	0xbbaf0000
-#define DDR_BASE		0xc0000000
+#define DDR_BASE		0x93c00000
 #define PCI_MMIO_BASE		0xff000000
 
 #endif /* _MEMORY_H_ */
