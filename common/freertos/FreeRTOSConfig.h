678ea49576a3b53e9ce9f7b5b787f2b01fa66fb9
Nicolas Diaz <nicolas.diaz@nxp.com>
2022-04-21 11:50:44 +0200

    freertos: Align priorities with GenAVB/TSN project
    Signed-off-by: Nicolas Diaz <nicolas.diaz@nxp.com>
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

--- common/freertos/FreeRTOSConfig.h
+++ common/freertos/FreeRTOSConfig.h
@@ -80,7 +80,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define configUSE_TICKLESS_IDLE                 0
 #define configCPU_CLOCK_HZ                      (SystemCoreClock)
 #define configTICK_RATE_HZ                      ((TickType_t)1000)
-#define configMAX_PRIORITIES                    5
+#define configMAX_PRIORITIES                    16
 #define configMINIMAL_STACK_SIZE                ((unsigned short)90)
 #define configMAX_TASK_NAME_LEN                 20
 #define configUSE_16_BIT_TICKS                  0
