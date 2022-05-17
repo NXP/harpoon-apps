f71148f173059b60a4135a944a3cb03164cc3936
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-05-17 09:27:56 +0800

    common: zephyr: add dead loop in os_assert()
    Harpoon application exepect a dead loop in case of os_assert(false,..),
    but Zephyr's os_assert may be not stopped in case of false condition,
    so add a fixed dead loop in Zephyr's os_assert.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- common/zephyr/os/assert.h
+++ common/zephyr/os/assert.h
@@ -8,8 +8,14 @@
 
 #include <sys/__assert.h>
 
-#define os_assert(cond, msg, ...) __ASSERT(cond, msg, ##__VA_ARGS__)
+#define os_assert(cond, msg, ...)				\
+	do {							\
+		if (unlikely(!(cond))) {			\
+			__ASSERT(cond, msg, ##__VA_ARGS__);	\
+			while(1);				\
+		}						\
+	} while(0)
 
-#define os_assert_equal(a, b, msg, ...) __ASSERT((a) == (b), msg, ##__VA_ARGS__)
+#define os_assert_equal(a, b, msg, ...)      os_assert((a) == (b), msg, ##__VA_ARGS__)
 
 #endif /* #ifndef _ZEPHYR_ASSERT_H_ */
