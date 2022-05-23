bf0241ec1bd1b51b1cdf10ea175c696f0bab910f
Stephane Viau <stephane.viau@nxp.com>
2022-05-23 04:56:16 +0000

    industrial: Add board definition for AVB/TSN stack
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

---
+++ industrial/common/boards/evkmimx8mm/app_board.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _APP_BOARD_H_
+#define _APP_BOARD_H_
+
+#include "fsl_enet.h"
+#include "fsl_enet_mdio.h"
+#include "fsl_phyar8031.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+#define BOARD_NUM_PORTS      1
+/* PHY configuration */
+#define BOARD_NUM_PHY        1
+
+/* PHY 0 is RTL8211FDI phy */
+#define BOARD_PHY0_ADDRESS      (0x00U)           /* Phy address of enet port 0. */
+#define BOARD_PHY0_MDIO_BASE    ENET_BASE         /* MDIO MAC controller base address. */
+#define BOARD_PHY0_MDIO_OPS     &enet_ops         /* MDIO MAC controller operations. */
+#define BOARD_PHY0_OPS          &phyar8031_ops    /* PHY operations. */
+
+#define BOARD_PHY0_TX_LATENCY_100M (810)          /* FIXME - Needs calibration */
+#define BOARD_PHY0_RX_LATENCY_100M (810)          /* FIXME - Needs calibration */
+
+/* Use ENET QOS */
+#define BOARD_USE_ENET_QOS      (0)
+
+#define BOARD_ENET0_MII_MODE    kENET_RgmiiMode
+#define BOARD_ENET0_DRV_TYPE    ENET_t
+#define BOARD_ENET0_DRV_BASE    ENET_BASE
+#define BOARD_ENET0_DRV_IRQ     ENET_IRQn
+
+#define BOARD_ENET0_1588_TIMER_CHANNEL_0      kENET_PtpTimerChannel1
+#define BOARD_ENET0_1588_TIMER_CHANNEL_1      kENET_PtpTimerChannel3
+#define BOARD_ENET0_1588_TIMER_CHANNEL_2      kENET_PtpTimerChannel4
+#define BOARD_ENET0_1588_TIMER_EVENT_CHANNEL  kENET_PtpTimerChannel2
+
+#define BOARD_ENET0_PHY_INDEX   (0)
+
+#define BOARD_GPT_0_BASE        GPT2
+#define BOARD_GPT_0_IRQ         GPT2_IRQn
+
+#define BOARD_GPT_1_BASE        GPT1
+#define BOARD_GPT_1_IRQ         GPT1_IRQn
+
+#define BOARD_GPT_1_CHANNEL          kGPT_InputCapture_Channel2
+#define BOARD_GPT_1_INTERRUPT_MASK   kGPT_InputCapture2InterruptEnable
+#define BOARD_GPT_1_STATUS_FLAG_MASK kGPT_InputCapture2Flag
+
+#define BOARD_GPT_1_CLK_EXT_FREQ    24000000U
+#define BOARD_GPT_1_CLK_SOURCE_TYPE kGPT_ClockSource_Periph
+
+#endif /* _APP_BOARD_H_ */
