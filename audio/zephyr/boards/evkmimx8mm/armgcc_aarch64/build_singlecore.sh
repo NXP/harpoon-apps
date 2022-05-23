b0b2be9ef821c76c20a9f127231d2808b7460f36
Fabrice Goucem <fabrice.goucem@nxp.com>
2022-05-23 15:23:24 +0200

    audio: add build scripts for Zephyr
    Scripts build Zephyr without "west" machinery.
    
    Signed-off-by: Fabrice Goucem <fabrice.goucem@nxp.com>

---
+++ audio/zephyr/boards/evkmimx8mm/armgcc_aarch64/build_singlecore.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+CMSIS_PATH=$(realpath ../../../../../../modules/hal/cmsis)
+HAL_NXP_PATH=$(realpath ../../../../../../modules/hal/nxp)
+
+ZEPHYR_TOOLCHAIN_VARIANT=cross-compile
+CROSS_COMPILE=$(find "$ARMGCC_DIR" -name "*-gcc" | sed -e 's/gcc$//')
+export ZEPHYR_TOOLCHAIN_VARIANT
+export CROSS_COMPILE
+
+cmake -B build_singlecore -GNinja -DZEPHYR_MODULES="$CMSIS_PATH;$HAL_NXP_PATH" -DBOARD=mimx8mm_evk_a53 ../../../
+ninja -C build_singlecore
