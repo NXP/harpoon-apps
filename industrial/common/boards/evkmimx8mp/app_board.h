c81d1a00e4da858c6df23ce036d7143e23956868
Rui Sousa <rui.sousa@nxp.com>
2022-06-11 17:25:53 +0200

    industrial: Board file cleanup
    Adjust board file content to match lastest gen_avb version.
    Remove ENET PHY configuration since it's not usable on RTOS side.
    
    Signed-off-by: Rui Sousa <rui.sousa@nxp.com>

--- industrial/common/boards/evkmimx8mp/app_board.h
+++ industrial/common/boards/evkmimx8mp/app_board.h
@@ -16,38 +16,28 @@
 #define BOARD_NET_TX_CACHEABLE
 #define BOARD_NET_RX_CACHEABLE
 
-#define BOARD_NUM_PORTS      1
 /* PHY configuration */
-#define BOARD_NUM_PHY        2
+#define BOARD_NUM_PHY        1
 
 /* PHY 0 is RTL8211FDI phy */
-#define BOARD_PHY0_ADDRESS      (0x02U)           /* Phy address of enet port 0. */
-#define BOARD_PHY0_MDIO_BASE    ENET1_BASE         /* MDIO MAC controller base address. */
-#define BOARD_PHY0_MDIO_OPS     &enet_ops         /* MDIO MAC controller operations. */
-#define BOARD_PHY0_OPS          &phyrtl8211f_ops   /* PHY operations. */
+#define BOARD_PHY0_ADDRESS      (0x01U)           /* Phy address of enet port 1. */
+#define BOARD_PHY0_MDIO_BASE    ENET_QOS_BASE     /* MDIO MAC controller base address. */
+#define BOARD_PHY0_MDIO_OPS     &enet_qos_ops     /* MDIO MAC controller operations. */
+#define BOARD_PHY0_OPS          &phyrtl8211f_ops  /* PHY operations. */
 
 #define BOARD_PHY0_TX_LATENCY_100M (650) /* FIXME - Needs calibration, comes from RT1170 which has same phy */
 #define BOARD_PHY0_RX_LATENCY_100M (650) /* FIXME - Needs calibration, comes from RT1170 which has same phy */
 #define BOARD_PHY0_TX_LATENCY_1G   (184)
 #define BOARD_PHY0_RX_LATENCY_1G   (569)
 
-/* Use ENET QOS */
-#define BOARD_USE_ENET_QOS      (1)
-
-/* PHY 1 is RTL8211FDI phy */
-#define BOARD_PHY1_ADDRESS      (0x01U)           /* Phy address of enet port 1. */
-#define BOARD_PHY1_MDIO_BASE    ENET_QOS_BASE     /* MDIO MAC controller base address. */
-#define BOARD_PHY1_MDIO_OPS     &enet_qos_ops     /* MDIO MAC controller operations. */
-#define BOARD_PHY1_OPS          &phyrtl8211f_ops  /* PHY operations. */
-
-#define BOARD_PHY1_TX_LATENCY_100M (650) /* FIXME - Needs calibration, comes from RT1170 which has same phy */
-#define BOARD_PHY1_RX_LATENCY_100M (650) /* FIXME - Needs calibration, comes from RT1170 which has same phy */
-#define BOARD_PHY1_TX_LATENCY_1G   (184)
-#define BOARD_PHY1_RX_LATENCY_1G   (569)
+/* Network Port configuration */
+#define BOARD_NUM_PORTS                1
+#define BOARD_NUM_ENET_QOS_PORTS       1
 
 #define BOARD_ENET0_MII_MODE    kENET_QOS_RgmiiMode
 #define BOARD_ENET0_DRV_TYPE    ENET_QOS_t
 #define BOARD_ENET0_DRV_BASE    ENET_QOS_BASE
+#define BOARD_ENET0_DRV_INDEX	(0)
 #define BOARD_ENET0_DRV_IRQ     ENET_QOS_IRQn
 #define BOARD_ENET0_DRV_IRQ_HND ENET_QOS_DriverIRQHandler
 
@@ -58,19 +48,7 @@
 
 #define BOARD_ENET0_1588_TIMER_PPS            kENET_QOS_PtpPpsIstance3 /* FIXME - Need to identify timer channel with PPS output */
 
-#define BOARD_ENET0_PHY_INDEX   (1)
-
-#define BOARD_ENET1_MII_MODE    kENET_RmiiMode
-#define BOARD_ENET1_DRV_TYPE    ENET_t
-#define BOARD_ENET1_DRV_BASE    ENET1_BASE
-#define BOARD_ENET1_DRV_IRQ     ENET1_IRQn
-
-#define BOARD_ENET1_1588_TIMER_CHANNEL_0      kENET_PtpTimerChannel1
-#define BOARD_ENET1_1588_TIMER_CHANNEL_1      kENET_PtpTimerChannel3
-#define BOARD_ENET1_1588_TIMER_CHANNEL_2      kENET_PtpTimerChannel4
-#define BOARD_ENET1_1588_TIMER_EVENT_CHANNEL  kENET_PtpTimerChannel2
-
-#define BOARD_ENET1_PHY_INDEX   (0)
+#define BOARD_ENET0_PHY_INDEX   (0)
 
 #define BOARD_GPT_0_BASE        GPT2
 #define BOARD_GPT_0_IRQ         GPT2_IRQn
