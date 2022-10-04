aa47fa1055007390b0670d37bfbd7cc00176f7a5
Stephane Viau <stephane.viau@nxp.com>
2022-10-04 15:59:05 +0000

    audio: freertos: 8m{m,n}: Add MMU entry for ENET
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

--- audio/freertos/boards/evkmimx8mm/app_mmu.h
+++ audio/freertos/boards/evkmimx8mm/app_mmu.h
@@ -9,6 +9,11 @@
 #define _APP_MMU_H_
 
 #define APP_MMU_ENTRIES		\
+									\
+	MMU_REGION_FLAT_ENTRY("ENET",				\
+			      ENET_BASE, KB(64),			\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
+									\
 	MMU_REGION_FLAT_ENTRY("I2C3",					\
 			      I2C3_BASE, KB(64),			\
 			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
