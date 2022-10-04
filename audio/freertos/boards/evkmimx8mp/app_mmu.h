4487539bb94c27c57ffcd3202985a8fdb2f05782
Stephane Viau <stephane.viau@nxp.com>
2022-10-04 16:09:43 +0000

    audio: freertos: 8mp: Add MMU entry for ENET_QOS
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

--- audio/freertos/boards/evkmimx8mp/app_mmu.h
+++ audio/freertos/boards/evkmimx8mp/app_mmu.h
@@ -13,17 +13,25 @@
 			      AUDIOMIX_BASE, KB(64),			\
 			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
+	MMU_REGION_FLAT_ENTRY("ENET_QOS",				\
+			      ENET_QOS_BASE, KB(64),			\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),  \
+									\
 	MMU_REGION_FLAT_ENTRY("GPC",					\
 			      GPC_BASE, KB(64),				\
 			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
 									\
-	MMU_REGION_FLAT_ENTRY("GPIO5",					\
-			      GPIO5_BASE, KB(64),			\
-			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
+	MMU_REGION_FLAT_ENTRY("GPIO4",					\
+			      GPIO4_BASE, KB(64),			\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),  \
 									\
 	MMU_REGION_FLAT_ENTRY("I2C3",					\
 			      I2C3_BASE, KB(64),			\
 			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),	\
+									\
+	MMU_REGION_FLAT_ENTRY("IOMUXC_GPR",				\
+			      IOMUXC_GPR_BASE, KB(64),			\
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),  \
 									\
 	MMU_REGION_FLAT_ENTRY("SAI1",					\
 			      I2S1_BASE, KB(64),			\
