9a1cfb5be9b197784a229fe6a7dc0016fa2d36dd
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2022-10-13 21:51:08 +0800

    ctrl: build applications for both IVSHMEM and RPMSG as mailbox transport layer
    The image harpoon_ctrl is for IVSHMEM as transport layer, and
    harpoon_ctrl_rpmsg is for RPMSG transport layer.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- ctrl/build_ctrl.sh
+++ ctrl/build_ctrl.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
-if [ -f "Makefile" ];then rm -f Makefile; fi
-if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
-if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -G "Unix Makefiles"  .
+
+cmake_clean ()
+{
+	if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
+	if [ -f "Makefile" ];then rm -f Makefile; fi
+	if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
+	if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
+}
+
+cmake_clean
+cmake -G "Unix Makefiles" -DMBOX_TRANSPORT_RPMSG=off .
+make -j $(nproc)
+
+cmake_clean
+cmake -G "Unix Makefiles" -DMBOX_TRANSPORT_RPMSG=on .
 make -j $(nproc)
