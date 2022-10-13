79933eb49895b1516e84623ad9a47a2a4cf16488
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2022-10-13 23:09:30 +0800

    rt_latency: freertos: evkmimx8mm: Add image build of RPMSG as transport layer
    The name with '_rpmsg' is the image using RPMSG as transport layer of
    harpoon control.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- rt_latency/freertos/boards/evkmimx8mm/armgcc_aarch64/build_ddr_debug.sh
+++ rt_latency/freertos/boards/evkmimx8mm/armgcc_aarch64/build_ddr_debug.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
-if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
-if [ -f "Makefile" ];then rm -f Makefile; fi
-if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
-if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../mcux-sdk/tools/cmake_toolchain_files/armgcc_aarch64.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_debug  .
+
+cmake_clean ()
+{
+	if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
+	if [ -f "Makefile" ];then rm -f Makefile; fi
+	if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
+	if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
+}
+
+cmake_clean
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../mcux-sdk/tools/cmake_toolchain_files/armgcc_aarch64.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_debug -DMBOX_TRANSPORT_RPMSG=off .
+make -j $(nproc)
+
+cmake_clean
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../mcux-sdk/tools/cmake_toolchain_files/armgcc_aarch64.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_debug -DMBOX_TRANSPORT_RPMSG=on .
 make -j $(nproc)
