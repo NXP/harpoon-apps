3aab5268074dea10483891f5c4d0c84cfcc13d1d
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2023-01-18 20:11:08 +0800

    common: freertos: add evkmimx93 board support
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

---
+++ common/freertos/boards/evkmimx93/board.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#if __has_include("app_board.h")
+#include "app_board.h"
+#endif
+#include "fsl_clock.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*! @brief The board name */
+#define BOARD_NAME        "MIMX93-EVK"
+#define MANUFACTURER_NAME "NXP"
+#define BOARD_DOMAIN_ID   (1U)
+/* The UART to use for debug messages. */
+#define BOARD_DEBUG_UART_TYPE     kSerialPort_Uart
+#define BOARD_DEBUG_UART_BAUDRATE (115200U)
+#define BOARD_DEBUG_UART_BASEADDR LPUART2_BASE
+#define BOARD_DEBUG_UART_INSTANCE (2U)
+#define BOARD_DEBUG_UART_CLK_FREQ (CLOCK_GetIpFreq(kCLOCK_Root_Lpuart2))
+#define BOARD_UART_IRQ            LPUART2_IRQn
+#define BOARD_UART_IRQ_HANDLER    LPUART2_IRQHandler
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* __cplusplus */
+
+/*******************************************************************************
+ * API
+ ******************************************************************************/
+
+void BOARD_InitDebugConsole(void);
+void BOARD_InitMemory(void);
+void BOARD_RdcInit(void);
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+
+#endif /* _BOARD_H_ */
