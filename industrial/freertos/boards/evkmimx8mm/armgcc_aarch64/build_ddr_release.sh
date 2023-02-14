42976852767d08dafa1e296224c3d2a209d14387
Zineb El Azzouzi <zineb.elazzouzi@nxp.com>
2023-02-14 15:33:01 +0100

    industrial: freertos: add RPMSG to imx8mn and imx8mp
    Signed-off-by: Zineb El Azzouzi <zineb.elazzouzi@nxp.com>

--- industrial/freertos/boards/evkmimx8mm/armgcc_aarch64/build_ddr_release.sh
+++ industrial/freertos/boards/evkmimx8mm/armgcc_aarch64/build_ddr_release.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-cmake_clean ()
-{
-	if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
-	if [ -f "Makefile" ];then rm -f Makefile; fi
-	if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
-	if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-}
+if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
+if [ -f "Makefile" ];then rm -f Makefile; fi
+if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
+if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
 
-cmake_clean
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../mcux-sdk/tools/cmake_toolchain_files/armgcc_aarch64.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_release -DMBOX_TRANSPORT_RPMSG=off .
-make -j $(nproc)
-
-cmake_clean
-cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../mcux-sdk/tools/cmake_toolchain_files/armgcc_aarch64.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_release -DMBOX_TRANSPORT_RPMSG=on .
+cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../../mcux-sdk/tools/cmake_toolchain_files/armgcc_aarch64.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=ddr_release .
 make -j $(nproc)
