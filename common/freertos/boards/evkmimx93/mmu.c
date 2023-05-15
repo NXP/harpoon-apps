7663aa3026dc35149de19b4667c6bb3d1e65a31d
Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
2023-05-15 20:02:41 +0200

    rt_latency: freertos: add imx93evk support
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>

--- common/freertos/boards/evkmimx93/mmu.c
+++ common/freertos/boards/evkmimx93/mmu.c
@@ -98,6 +98,14 @@ static const struct ARM_MMU_region mmu_regions[] = {
 			      GIC_DISTRIBUTOR_BASE, KB(1024),
 			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
 
+	MMU_REGION_FLAT_ENTRY("TPM2",
+			      TPM2_BASE, KB(64),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("TPM4",
+			      TPM4_BASE, KB(64),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
 	MMU_REGION_FLAT_ENTRY("IOMUXC",
 			      IOMUXC1_BASE, KB(64),
 			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
