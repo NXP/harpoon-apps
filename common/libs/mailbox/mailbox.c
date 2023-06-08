64a4b2939297f96ab7595074e77d195a79178998
Zineb El Azzouzi <zineb.elazzouzi@nxp.com>
2023-06-08 11:41:05 +0200

    common: mailbox: delete ivshmem to keep only RPMsg use
    Signed-off-by: Zineb El Azzouzi <zineb.elazzouzi@nxp.com>

--- common/libs/mailbox/mailbox.c
+++ common/libs/mailbox/mailbox.c
@@ -7,18 +7,10 @@
 #include <stdint.h>
 #include <string.h>
 
-#ifdef MBOX_TRANSPORT_RPMSG
 #include "rpmsg.h"
-#endif
-
 #include "mailbox.h"
 
-
 #define MAX_PAYLOAD	255
-
-#ifndef __DSB
-#define __DSB()	__asm volatile ("dsb sy")
-#endif
 
 struct cmd {
 	volatile uint32_t seq;
@@ -47,16 +39,8 @@ int mailbox_cmd_send(struct mailbox *mbox, void *data, unsigned int len)
 	c->len = len;
 	memcpy(c->data, data, len);
 
-#ifndef MBOX_TRANSPORT_RPMSG
-	mbox->last_cmd += 1;
-
-	__DSB();
-
-	c->seq = mbox->last_cmd;
-#else
 	if (rpmsg_send(mbox->transport, c, sizeof(*c)))
 		return -3;
-#endif
 
 	return 0;
 }
@@ -69,22 +53,8 @@ int mailbox_resp_recv(struct mailbox *mbox, void *data, unsigned int *len)
 	if (!mbox->dir)
 		return -1;
 
-#ifdef MBOX_TRANSPORT_RPMSG
 	if (rpmsg_recv(mbox->transport, r, sizeof(*r)))
 		return -3;
-#else
-	/* check if new response */
-	if (r->seq == mbox->last_resp)
-		return -1;
-
-	mbox->last_resp = r->seq;
-
-	__DSB();
-
-	/* check if it matches command */
-	if (r->seq != mbox->last_cmd)
-		return -2;
-#endif
 
 	resp_len = r->len;
 
@@ -105,18 +75,8 @@ int mailbox_cmd_recv(struct mailbox *mbox, void *data, unsigned int *len)
 	if (mbox->dir)
 		return -1;
 
-#ifdef MBOX_TRANSPORT_RPMSG
 	if (rpmsg_recv(mbox->transport, c, sizeof(*c)))
 		return -3;
-#else
-	/* check if new command */
-	if (c->seq == mbox->last_cmd)
-		return -1;
-
-	mbox->last_cmd = c->seq;
-
-	__DSB();
-#endif
 
 	cmd_len = c->len;
 
@@ -142,14 +102,8 @@ int mailbox_resp_send(struct mailbox *mbox, void *data, unsigned int len)
 	r->len = len;
 	memcpy(r->data, data, len);
 
-#ifndef MBOX_TRANSPORT_RPMSG
-	__DSB();
-
-	r->seq = mbox->last_cmd;
-#else
 	if (rpmsg_send(mbox->transport, r, sizeof(*r)))
 		return -3;
-#endif
 
 	return 0;
 }
