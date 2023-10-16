a07793e966226ecf7315fe742f4e97504f2f29b6
Aziz Sellami <aziz.sellami@nxp.com>
2023-10-16 14:57:43 +0200

    industrial: tsn_app: adapt TickType_T handling
    Signed-off-by: Aziz Sellami <aziz.sellami@nxp.com>

--- industrial/avb_tsn/tsn_app/alarm_task.c
+++ industrial/avb_tsn/tsn_app/alarm_task.c
@@ -16,7 +16,7 @@ static void net_callback(void *data)
     struct tsn_task *task = container_of(sock, struct tsn_task, sock_rx[sock->id]);
     struct alarm_task *a_task = task->ctx;
 
-    if (xQueueSend(a_task->queue.handle, &sock, 0) != pdTRUE) {
+    if (xQueueSend(a_task->queue.handle, &sock, pdMS_TO_TICKS(0)) != pdTRUE) {
         ERR("xQueueSendFromISR() failed\n\r");
     }
 }
