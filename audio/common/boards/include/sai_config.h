3b779ab9447b3244f2499fec18fe20ca5ad8e01d
Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
2023-10-24 20:17:10 +0200

    audio: common: make audio_pll field generic
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>

--- audio/common/boards/include/sai_config.h
+++ audio/common/boards/include/sai_config.h
@@ -10,7 +10,7 @@ struct sai_active_config {
 	void *sai_base;
 	uint32_t masterSlave;
 	uint32_t msel;
-	clock_pll_ctrl_t audio_pll;
+	uint32_t audio_pll;
 	uint32_t audio_pll_mul;
 	uint32_t audio_pll_div;
 	sai_sync_mode_t rx_sync_mode;
