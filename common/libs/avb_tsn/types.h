7bcb0eb15abf06cb93a296c9c5fbc59f821dac25
Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
2024-03-12 17:01:42 +0100

    common: avb_tsn: use offsetof() from standard c lib rather than a custom implementation
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>

--- common/libs/avb_tsn/types.h
+++ common/libs/avb_tsn/types.h
@@ -1,11 +1,13 @@
 /*
- * Copyright 2018 NXP
+ * Copyright 2018, 2024 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #ifndef _TYPES_H_
 #define _TYPES_H_
+
+#include <stddef.h>     /* for offsetof() */
 
 #define NSECS_PER_SEC  (1000000000ULL)
 #define NSECS_PER_MSEC (1000000)
@@ -14,7 +16,6 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
-#define offset_of(type, member)           ((unsigned long)&(((type *)0)->member))
-#define container_of(entry, type, member) ((type *)((unsigned char *)(entry)-offset_of(type, member)))
+#define container_of(entry, type, member) ((type *)((unsigned char *)(entry) - offsetof(type, member)))
 
 #endif /* _TYPES_H_*/
