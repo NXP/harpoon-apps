a0a0fae364d3ab1b06e40e3702076a6892c94903
Aziz Sellami <aziz.sellami@nxp.com>
2025-09-17 11:51:04 +0200

    audio: zephyr: move to ZSDK
    Signed-off-by: Aziz Sellami <aziz.sellami@nxp.com>

--- audio/zephyr/boards/evkmimx8mp/armgcc_aarch64/build_smp.sh
+++ audio/zephyr/boards/evkmimx8mp/armgcc_aarch64/build_smp.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-CMSIS_PATH=$(realpath ../../../../../../modules/hal/cmsis)
-HAL_NXP_PATH=$(realpath ../../../../../../modules/hal/nxp)
 
 ZEPHYR_TOOLCHAIN_VARIANT=cross-compile
 CROSS_COMPILE=$(find "$ARMGCC_DIR" -name "*-gcc" | sed -e 's/gcc$//')
 export ZEPHYR_TOOLCHAIN_VARIANT
 export CROSS_COMPILE
 
-cmake -B build_smp -GNinja -DZEPHYR_MODULES="$CMSIS_PATH;$HAL_NXP_PATH" -DBOARD=imx8mp_evk/mimx8ml8/a53/smp ../../../
-ninja -C build_smp
+west build -p always ../../../ -b imx8mp_evk/mimx8ml8/a53/smp -d build_smp
