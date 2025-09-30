b1fb729753b6ea41dbfdb47583af0de08470414c
Aziz Sellami <aziz.sellami@nxp.com>
2025-09-30 10:10:09 +0200

    audio: freertos: disable array-bounds error
    Fix following compile issue:
    """
    In function 'ENET_SetTsISRHandler',
        inlined from 'ENET_Ptp1588ConfigureHandler' at
    /home/nxf95737/Projects/HRPN/main-ng/workspace/mcuxsdk/mcuxsdk/drivers/enet/fsl_enet.c:2890:5:
    /home/nxf95737/Projects/HRPN/main-ng/workspace/mcuxsdk/mcuxsdk/drivers/enet/fsl_enet.c:458:16:
    error: array subscript 2 is above array bounds of 'void (*[2])(ENET_Type
    *, enet_handle_t *)' {aka 'void (*[2])(ENET_Type *, struct _enet_handle
    *)'} [-Werror=array-bounds=]
      458 |     s_enetTsIsr[instance] = ISRHandler;
          |     ~~~~~~~~~~~^~~~~~~~~~
    """
    Due to assert in mcuxsdk-core/drivers/enet/fsl_enet.c#194 (in
    ENET_GetInstance) being skipped by enabling '-DNDEBUG'.
    Disabling this warning allows compilation.
    
    Signed-off-by: Aziz Sellami <aziz.sellami@nxp.com>

--- audio/freertos/genavb_sdk.cmake
+++ audio/freertos/genavb_sdk.cmake
@@ -2,6 +2,9 @@ mcux_set_variable(RTOS_ABSTRACTION_LAYER_DIR "${SdkRootDirPath}/../../rtos-abstr
 mcux_set_variable(GenAVBPath "${SdkRootDirPath}/../../gen_avb_sdk")
 mcux_set_variable(CONFIG "endpoint_avb")
 
+# Disable "array-bounds" that leads to an error when using "DNDEBUG", due to a skipped assert in fsl_enet.c in ENET_SetTsISRHandler.
+set_source_files_properties(${SdkRootDirPath}/drivers/enet/fsl_enet.c PROPERTIES COMPILE_FLAGS "-Wno-array-bounds")
+
 include(${GenAVBPath}/mcux_genavb.cmake)
 include(${SdkRootDirPath}/${harpoon_root_path}/common/libs/avb_tsn/lib_avb_tsn.cmake)
 
